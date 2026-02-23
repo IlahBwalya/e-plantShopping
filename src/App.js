28dd67472d64bb5a7be056fe0efc6c7b75a1a69d
Ilah Bwalya <ilahbwalya7@gmail.com>
2026-02-23 23:13:42 +0530

    Added landing page background

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import Navbar from "./components/Navbar";
 import ProductList from "./pages/ProductList";
 import CartItem from "./pages/CartItem";
 import AboutUs from "./pages/AboutUs";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<ProductList />} />
+        <Route
+          path="/"
+          element={
+            <div className="landing-page">
+              <div className="landing-content">
+                <h1>Welcome to Paradise Nursery</h1>
+                <p>Your one-stop shop for beautiful plants</p>
+              </div>
+            </div>
+          }
+        />
+        <Route path="/products" element={<ProductList />} />
         <Route path="/cart" element={<CartItem />} />
         <Route path="/about" element={<AboutUs />} />
       </Routes>
